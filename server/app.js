fafb153d1d32952b97bc7f4f476b47baa043c482
Jeff Soto <jeff@turkoisdesign.com>
2018-12-27 15:01:44 -0500

    integrate Mongoose and MongoDB

--- server/app.js
+++ server/app.js
@@ -2,26 +2,34 @@ const express = require('express');
 app = express();
 const mongoose = require('mongoose');
 
-const users = [
-  {
-    name: 'Jeff',
-    age: 29,
-    gender: 'Male'
-  },
-  {
-    name: 'Jay',
-    age: 30,
-    gender: 'Male'
-  },
-  {
-    name: 'Aly',
-    age: 27,
-    gender: 'Female'
-  }
-]
+mongoose.Promise = Promise;
 
-app.get('/api/users', (req, res) => {
-  res.send(users);
+mongoose.connect("mongodb://localhost/weed-deliver", { useNewUrlParser: true })
+.then(res => console.log("Connected to DB"))
+  .catch(err => console.log("Error connecting to DB: ", err))
+
+const deliverySchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  notes: String
+});
+
+const Delivery =  mongoose.model("Delivery", deliverySchema);
+
+app.get('/api/deliveries', (req, res) => {
+  Delivery.find({}, (err, allDeliveries)=> {
+    if (err){
+      console.log("Error finding all deliveries: ", err);
+      res.send("Error");
+      return;
+    }
+    res.send(allDeliveries);
+  });
+});
+
+app.post('/api/deliveries', (req, res) => {
+  const deliveryInfo = req.body.delivery;
+  console.log("Delivery Info", deliveryInfo);
 });
 
 app.listen(3000, () => {
